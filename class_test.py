20115f24d18c0e61e61084e59e4a4a319d08b467
Cayden Mclendon <cmclendon2223@floridapoly.edu>
2026-01-17 17:29:11 +0000

    Added print functions

--- class_test.py
+++ class_test.py
@@ -1,9 +1,10 @@
+# save name to a string variable
 my_name = "Cayden Mclendon"
 
+# print name to console
 print(my_name)
 
+# print other information to console
 print("Use 4 spaces per indentation level.")
-
 print("CPython is the original and most maintained implementation of Python.")
-
 print("With this course, I hope to become better at problem solving, learn more Python, and gain experience with git and GitHub.")
