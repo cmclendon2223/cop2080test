e02abbeac2b7de538853f9bb3d1848eb1e0132b3
Cayden Mclendon <cmclendon2223@floridapoly.edu>
2026-01-17 17:24:26 +0000

    Added print function

---
+++ class_test.py
@@ -0,0 +1,9 @@
+my_name = "Cayden Mclendon"
+
+print(my_name)
+
+print("Use 4 spaces per indentation level.")
+
+print("CPython is the original and most maintained implementation of Python.")
+
+print("With this course, I hope to become better at problem solving, learn more Python, and gain experience with git and GitHub.")
--- print_test.py
+++
@@ -1,5 +0,0 @@
-s = "The trouble is"
-
-print(s[0])
-
-print("second test")
